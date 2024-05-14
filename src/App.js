2daa3deb623010820f8a4d935280358bfc1dd1d4
Steve Frost <RURational@gmail.com>
2024-05-13 21:40:25 -0400

    Tweak slightly for FrostOps

--- src/App.js
+++ src/App.js
@@ -7,7 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          FrostOps is a web application for managing Frost operations.
+        </p>
+        <p>
+          It is a work in progress and under construction.
         </p>
         <a
           className="App-link"
